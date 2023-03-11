79d6d3fbdbbff2aea4b1c6d800c1da4a9ca4cd27
Qreamville <Oyeleyebalikis@yahoo.com>
2023-03-11 17:39:49 +0100

    feat: lesson 1 part 2 update

--- lesson1/main.js
+++ lesson1/main.js
@@ -1,29 +1,36 @@
 import gsap from "gsap";
 
-const playBtn = document.getElementById("play");
-const pauseBtn = document.getElementById("pause");
-const reverseBtn = document.getElementById("reverse");
-const restartBtn = document.getElementById("restart");
+// Tweens
+// const playBtn = document.getElementById("play");
+// const pauseBtn = document.getElementById("pause");
+// const reverseBtn = document.getElementById("reverse");
+// const restartBtn = document.getElementById("restart");
 
-let playAnimation = gsap.to(".img1", {
-  x: 800,
-  duration: 1,
-  ease: "linear",
-  paused: true,
-});
+// let playAnimation = gsap.to(".img1", {
+//   x: 500,
+//   duration: 1,
+//   ease: "linear",
+//   paused: true,
+// });
 
-playBtn.onclick = () => {
-  playAnimation.play();
-};
+// playBtn.onclick = () => {
+//   playAnimation.play();
+// };
 
-pauseBtn.onclick = () => {
-  playAnimation.pause();
-};
+// pauseBtn.onclick = () => {
+//   playAnimation.pause();
+// };
 
-reverseBtn.onclick = () => {
-  playAnimation.reverse();
-};
+// reverseBtn.onclick = () => {
+//   playAnimation.reverse();
+// };
 
-restartBtn.onclick = () => {
-  playAnimation.restart();
-};
+// restartBtn.onclick = () => {
+//   playAnimation.restart();
+// };
+gsap
+  .timeline()
+  .from("#app", { duration: 1, opacity: 0 })
+  .from(".img h1", { opacity: 0, ease: "back" })
+  .from(".img img", { y: 160, stagger: 0.1, duration: 0.8, ease: "back" })
+  .from(".time", { xPercent: 100, duration: 0.2 });
